07950d2afa5b626d0f80dba09c5c6abfd9f27ef1
wedw <wedw@gmail.com>
2022-02-17 16:22:34 +0100

    subida

---
+++ src/vscode2/Principal.java
@@ -0,0 +1,11 @@
+package vscode2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		String a = "Hola", b = "mundo";
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
